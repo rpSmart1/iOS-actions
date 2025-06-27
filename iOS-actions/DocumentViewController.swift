a850400d40f490719d3a4cb5cd0ec332d41568a6
Rudra Pratap Singh <95014241+rpSmart1@users.noreply.github.com>
2025-06-27 16:09:08 +0530

    print command added

--- iOS-actions/DocumentViewController.swift
+++ iOS-actions/DocumentViewController.swift
@@ -12,5 +12,6 @@ class DocumentViewController: UIDocumentViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("hello")
     }
 }
