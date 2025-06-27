711117bdb1c88afe8e6715cd2ce2c6e0f0596a9e
Rudra Pratap Singh <95014241+rpSmart1@users.noreply.github.com>
2025-06-27 14:47:12 +0530

    initial Commit

---
+++ iOS-actions/DocumentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DocumentViewController.swift
+//  iOS-actions
+//
+//  Created by Rudra Pratap Singh on 27/06/25.
+//
+
+import UIKit
+
+class DocumentViewController: UIDocumentViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ iOS-actionsUITests/iOS_actionsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_actionsUITestsLaunchTests.swift
+//  iOS-actionsUITests
+//
+//  Created by Rudra Pratap Singh on 27/06/25.
+//
+
+import XCTest
+
+final class iOS_actionsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
